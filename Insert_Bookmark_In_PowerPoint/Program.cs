21724d9d00a2e3c0ec3e2ef326a333b8a5b7ccba
Bhaskar365 <bhaskarjyotidas365@gmail.com>
2024-04-22 12:54:31 +0530

    code changes for bookmark

--- Insert_Bookmark_In_PowerPoint/Program.cs
+++ Insert_Bookmark_In_PowerPoint/Program.cs
@@ -1,56 +1,49 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using DocumentFormat.OpenXml.Drawing.Wordprocessing;
 using DocumentFormat.OpenXml.Packaging;
 
 Console.WriteLine("Hello, World!");
 
 static void AddBookmark(string file, string bookmarkName, int xPosition, int yPosition)
 {
-    using (var presentation = PresentationDocument.Open(file, true))
+    try
     {
-        Console.WriteLine("Bookmark add started");
-        // Get the first slide part
-        var slidePart = presentation.PresentationPart.SlideParts.First();
-
-        // Create a new shape tree
-        var tree = slidePart.Slide.Descendants<DocumentFormat.OpenXml.Presentation.ShapeTree>().First();
-
-        // Create a new bookmark shape
-        var bookmarkShape = new DocumentFormat.OpenXml.Presentation.Shape();
-
-        // Set non-visual properties of the bookmark
-        bookmarkShape.NonVisualShapeProperties = new DocumentFormat.OpenXml.Presentation.NonVisualShapeProperties();
-        bookmarkShape.NonVisualShapeProperties.Append(new DocumentFormat.OpenXml.Presentation.NonVisualDrawingProperties
+        using (var presentation = PresentationDocument.Open(file, true))
         {
-            Name = bookmarkName,
-            Id = (UInt32)tree.ChildElements.Count // Generate a unique Id for the bookmark
-        });
+            var slidePart = presentation.PresentationPart.GetPartsOfType<SlidePart>().FirstOrDefault();
+            if (slidePart != null)
+            {
+                var tree = slidePart.Slide.Descendants<DocumentFormat.OpenXml.Presentation.ShapeTree>().FirstOrDefault();
+                if (tree != null)
+                {
+                    var bookmarkShape = new DocumentFormat.OpenXml.Drawing.Shape(
+                        new DocumentFormat.OpenXml.Drawing.NonVisualShapeProperties(
+                            new DocumentFormat.OpenXml.Drawing.NonVisualDrawingProperties { Name = bookmarkName, Id = (UInt32)tree.ChildElements.Count },
+                            new DocumentFormat.OpenXml.Drawing.NonVisualShapeDrawingProperties(),
+                            new DocumentFormat.OpenXml.Office2010.Drawing.ChartDrawing.ApplicationNonVisualDrawingProperties()),
+                        new DocumentFormat.OpenXml.Drawing.ShapeProperties(
+                            new DocumentFormat.OpenXml.Drawing.Transform2D(new DocumentFormat.OpenXml.Drawing.Offset { X = xPosition, Y = yPosition }),
+                            new DocumentFormat.OpenXml.Drawing.PresetGeometry { Preset = DocumentFormat.OpenXml.Drawing.ShapeTypeValues.Rectangle },
+                            new DocumentFormat.OpenXml.Drawing.NoFill(),
+                            new Inline()));
 
-        // Set visual properties of the bookmark
-        bookmarkShape.ShapeProperties = new DocumentFormat.OpenXml.Presentation.ShapeProperties();
-        bookmarkShape.ShapeProperties.Transform2D = new DocumentFormat.OpenXml.Drawing.Transform2D();
-
-        // Set X-axis and Y-axis positions
-        bookmarkShape.ShapeProperties.Transform2D.Append(new DocumentFormat.OpenXml.Drawing.Offset
-        {
-            X = xPosition, // X-axis position
-            Y = yPosition, // Y-axis position
-        });
-
-        // Set the size and type of the bookmark
-        bookmarkShape.ShapeProperties.Transform2D.Append(new DocumentFormat.OpenXml.Drawing.Extents
-        {
-            Cx = 50, // Width of the bookmark (set to 0)
-            Cy = 50, // Height of the bookmark (set to 0)
-        });
-        bookmarkShape.ShapeProperties.Append(new DocumentFormat.OpenXml.Drawing.PresetGeometry
-        {
-            Preset = DocumentFormat.OpenXml.Drawing.ShapeTypeValues.Rectangle // Shape type
-        });
-
-        // Append the bookmark to the shape tree
-        tree.Append(bookmarkShape);
-        Console.WriteLine("Bookmark added");
+                    tree.AppendChild(bookmarkShape);
+                    Console.WriteLine("Bookmark added successfully!");
+                }
+                else
+                {
+                    Console.WriteLine("Slide does not contain a ShapeTree.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Presentation does not contain any slides.");
+            }
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Error adding bookmark: " + ex.Message);
     }
 }
-
 AddBookmark("C:\\OPENXML_C#\\Document_Generation_Using_OpenXML_With_Font_Syling\\Insert_Bookmark_In_PowerPoint\\PPTFolder\\blankPPTForBookmark.pptx", "MyBookmark", 200000, 200000);
--- Insert_Bookmark_In_PowerPoint/obj/Debug/net6.0/Insert_Bookmark_In_PowerPoint.AssemblyInfo.cs
+++ Insert_Bookmark_In_PowerPoint/obj/Debug/net6.0/Insert_Bookmark_In_PowerPoint.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Insert_Bookmark_In_PowerPoint")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f644fde6bc3ff09fc8b8deca22c0e61d50015b89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a87b50b042296da04e0a597c5805a048224d470")]
 [assembly: System.Reflection.AssemblyProductAttribute("Insert_Bookmark_In_PowerPoint")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Insert_Bookmark_In_PowerPoint")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
